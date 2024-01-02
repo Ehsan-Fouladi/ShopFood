6a262d3967c4eabaffc4c16d970144961f351a6a
Ehsan Fouladi <fooehan3@gmail.com>
2024-01-02 14:20:14 +0330

    remove database

--- Shop/settings.py
+++ Shop/settings.py
@@ -88,22 +88,22 @@ WSGI_APPLICATION = 'Shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', 
-        'NAME': 'mysqlvjt_db',
-        'USER': 'root',
-        'PASSWORD': 'eJ8b3VbuIWIv3Jd!jDqe',
-        'HOST': 'mysql-ndh-service',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', 
+#         'NAME': 'mysqlvjt_db',
+#         'USER': 'root',
+#         'PASSWORD': 'eJ8b3VbuIWIv3Jd!jDqe',
+#         'HOST': 'mysql-ndh-service',
+#         'PORT': '3306',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
